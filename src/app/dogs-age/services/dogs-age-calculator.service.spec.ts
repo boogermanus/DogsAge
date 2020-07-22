be54292f1192d4b03b4cc046da27a0dc492c5ccc
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-07-22 00:27:18 -0500

    test broken!

--- src/app/dogs-age/services/dogs-age-calculator.service.spec.ts
+++ src/app/dogs-age/services/dogs-age-calculator.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { DogsAgeCalculatorService } from './dogs-age-calculator.service';
+import { IDogsAge } from '../models/idogs-age';
+import { CalculationConstants } from '../models/calculation-constants';
+import { SizeConstants } from '../models/size-constants';
 
 describe('DogsAgeCalculatorService', () => {
   let service: DogsAgeCalculatorService;
+  let dog: IDogsAge;
 
   beforeEach(() => {
     TestBed.configureTestingModule({providers: [DogsAgeCalculatorService]});
     service = TestBed.inject(DogsAgeCalculatorService);
+    dog = {
+      calculationType: CalculationConstants.DEFAULT_VALUE,
+      age: 0,
+      size: SizeConstants.SMALL
+    }
   });
 
   it('should be created', () => {
@@ -14,10 +23,10 @@ describe('DogsAgeCalculatorService', () => {
   });
 
   it('should have method calculate', () => {
-    expect(() => service.calculate({
-      calculationType: 'c',
-      age: 0,
-      size: 's'
-    })).toBeTruthy();
+    expect(() => service.calculate(dog)).toBeTruthy();
+  });
+
+  it('should return 0 for classic', () => {
+    expect(() => service.calculate(dog)).toBe(0);
   });
 });
