ab9c59de85ffbb00174c21dab476719841a9de97
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-07-23 20:20:37 -0500

    more calculation and tests

--- src/app/dogs-age/services/dogs-age-calculator.service.spec.ts
+++ src/app/dogs-age/services/dogs-age-calculator.service.spec.ts
@@ -3,6 +3,7 @@ import { DogsAgeCalculatorService } from './dogs-age-calculator.service';
 import { IDogsAge } from '../models/idogs-age';
 import { CalculationConstants } from '../models/calculation-constants';
 import { SizeConstants } from '../models/size-constants';
+import { expressionType } from '@angular/compiler/src/output/output_ast';
 
 describe('DogsAgeCalculatorService', () => {
   let service: DogsAgeCalculatorService;
@@ -44,5 +45,57 @@ describe('DogsAgeCalculatorService', () => {
     dog.calculationType = CalculationConstants.NEW;
     dog.age = 1;
     expect(service.calculate(dog)).toBe(31);
+  });
+
+  it('should calculate new dogs age 42', () => {
+    dog.calculationType = CalculationConstants.NEW;
+    dog.age = 2;
+    expect(service.calculate(dog)).toBe(42);
+  });
+
+  it('should calculate new dogs age 0 for negative age', () => {
+    dog.calculationType = CalculationConstants.NEW;
+    dog.age = -1;
+    expect(service.calculate(dog)).toBe(0);
+  });
+
+  it('should calculate exotic dogs age 0', () => {
+    dog.calculationType = CalculationConstants.EXOTIC;
+    expect(service.calculate(dog)).toBe(0);
+  });
+
+  it('should calculate exotic dog age 0 for negative age', () => {
+    dog.calculationType = CalculationConstants.EXOTIC;
+    dog.age = -1;
+    expect(service.calculate(dog)).toBe(0);
+  });
+  it('should calculate exotic dog age 15', () => {
+    dog.calculationType = CalculationConstants.EXOTIC;
+    dog.age = 1;
+    expect(service.calculate(dog)).toBe(15);
+  });
+  it('should calculate exotic small dogs age 44', () => {
+    dog.calculationType = CalculationConstants.EXOTIC;
+    dog.size = SizeConstants.SMALL;
+    dog.age = 7;
+    expect(service.calculate(dog)).toBe(44);
+  });
+  it('should calculate exotic medium dogs age 47', () => {
+    dog.calculationType = CalculationConstants.EXOTIC;
+    dog.size = SizeConstants.MEDIUM;
+    dog.age = 7;
+    expect(service.calculate(dog)).toBe(47);
+  });
+  it('should calculate exotic medium dogs age 51', () => {
+    dog.calculationType = CalculationConstants.EXOTIC;
+    dog.size = SizeConstants.MEDIUM;
+    dog.age = 8;
+    expect(service.calculate(dog)).toBe(51);
+  });
+  it('should calculate exotic medium dogs age 56', () => {
+    dog.calculationType = CalculationConstants.EXOTIC;
+    dog.size = SizeConstants.MEDIUM;
+    dog.age = 9;
+    expect(service.calculate(dog)).toBe(56);
   });
 });
--- src/app/dogs-age/services/dogs-age-calculator.service.ts
+++ src/app/dogs-age/services/dogs-age-calculator.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { IDogsAge } from '../models/idogs-age';
 import { CalculationConstants } from '../models/calculation-constants';
+import { SizeConstants } from '../models/size-constants';
 
 @Injectable({
   providedIn: 'root'
@@ -8,18 +9,28 @@ import { CalculationConstants } from '../models/calculation-constants';
 export class DogsAgeCalculatorService {
 
   private readonly CLASSIC_AGE = 7;
+  private readonly EXOTIC_AGE = 15;
+  private readonly SMALL = 24;
 
   constructor() { }
 
   public calculate(options: IDogsAge): number {
+    let result = 0;
     switch (options.calculationType) {
       case CalculationConstants.CLASSIC:
-        return this.calculateClassic(options.age);
+        result = this.calculateClassic(options.age);
+        break;
       case CalculationConstants.NEW:
-        return this.calculateNew(options.age);
+        result = this.calculateNew(options.age);
+        break;
+      case CalculationConstants.EXOTIC:
+        result = this.calculateExotic(options);
+        break;
       default:
         return 0;
     }
+
+    return Math.round(result);
   }
 
   private calculateClassic(age: number): number {
@@ -27,8 +38,62 @@ export class DogsAgeCalculatorService {
   }
 
   private calculateNew(age: number): number {
-    if (age === 0) {
+    if (age <= 0) {
       return 0;
+    }
+
+    return 16 * Math.log(age) + 31;
+  }
+
+  private calculateExotic(options: IDogsAge): number {
+    if (options.age <= 0) {
+      return 0;
+    }
+
+    if (options.age === 1) {
+      return this.EXOTIC_AGE;
+    }
+
+    switch (options.size) {
+      case SizeConstants.SMALL:
+        return this.calculateExoticSmall(options.age);
+      case SizeConstants.MEDIUM:
+        return this.calculateExoticMedium(options.age);
+    }
+  }
+
+  private calculateExoticSmall(age: number): number {
+    switch (age) {
+      case 2:
+        return 24;
+      case 3:
+        return 28;
+      case 4:
+        return 32;
+      case 5:
+        return 36;
+      case 6:
+        return 40;
+      default:
+        return (age - 5) * 4 + 36;
+    }
+  }
+
+  private calculateExoticMedium(age: number): number {
+    switch (true) {
+      case (age < 6):
+        return this.calculateExoticSmall(age);
+      case (age === 6):
+        return 42;
+      case (age === 7):
+        return 47;
+      case (age > 7):
+        if (age % 2 === 0) {
+          return  (age - 6) / 2 * 9 + 42;
+        }
+        else {
+          return (age - 7) / 2 * 9 + 47;
+        }
     }
   }
 }
